3ed96854ea976320c022938ea48b3daa49533387
Danielle <danielle@natesdesign.com>
2020-11-16 13:02:45 -0700

    added comment and user

--- app.py
+++ app.py
@@ -26,30 +26,149 @@ class Post(db.Model):
 class Comment(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer)
+  content = db.Column(db.Text)
   post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-  post = db.relationship('Post',
-        backref=db.backref('comments', lazy=True))
 
-  def __init__(self, user_id, post_id, post):
+  def __init__(self, user_id, post_id, content):
     self.user_id = user_id
     self.post_id = post_id
-    self.post = post
+    self.content = content
   
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(25))
+  email = db.Column(db.String(40))
+  password = db.Column(db.String(40))
+  admin = db.Column(db.Boolean)
 
-  def __init__(self, name):
+  def __init__(self, name, email, password, admin):
     self.name = name
+    self.email = email
+    self.password = password
+    self.admin = admin
   
 
 class PostSchema(ma.Schema):
   class Meta:
-    fields = ("id", "title", "content")
+    fields = ("id", "title", "content", "user_id", "post_id", "post", "name", "comment")
+
+class CommentSchema(ma.Schema):
+  class Meta:
+    fields = ("id", "user_id", "post_id", "content")
+
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ("id", "name", "email", "password", "admin")
 
 post_schema = PostSchema()
 posts_schema = PostSchema(many=True)
+
+comment_schema = CommentSchema()
+comments_schema = CommentSchema(many=True)
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+# USERS
+
+@app.route("/user/<id>", methods=["DELETE"])
+def delete_user(id):
+  record = User.query.get(id)
+  db.session.delete(record)
+  db.session.commit()
+
+  return jsonify("USER DELETED")
+
+@app.route("/user/<id>", methods=["PATCH"])
+def update_user(id):
+  user = User.query.get(id)
+  rqt = request.get_json(force=True)
+
+  new_name = rqt["name"]
+  new_email = rqt["email"]
+  new_password = rqt["password"]
+  new_admin = rqt["admin"]
+
+  user.name = new_name
+  user.email = new_email
+  user.password = new_password
+  user.admin = new_admin
+  
+  db.session.commit()
+  print(user_schema.jsonify(user))
+  return user_schema.jsonify(user)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+  all_users = User.query.all()
+  result = users_schema.dump(all_users)
+  return jsonify(result)
+
+
+@app.route("/add-user", methods=["POST"])
+def add_user():
+  rqt = request.get_json(force=True)
+
+  name = rqt["name"]
+  email = rqt["email"]
+  password = rqt["password"]
+  admin = rqt["admin"]
+
+  record = User(name, email, password, admin)
+  db.session.add(record)
+  db.session.commit()
+
+  user = User.query.get(record.id)
+  return user_schema.jsonify(user)
+
+# COMMENTS
+
+@app.route("/comment/<id>", methods=["DELETE"])
+def delete_comment(id):
+  record = Comment.query.get(id)
+  db.session.delete(record)
+  db.session.commit()
+
+  return jsonify("COMMENT DELETED")
+
+@app.route("/comment/<id>", methods=["PATCH"])
+def update_comment(id):
+  comment = Comment.query.get(id)
+  rqt = request.get_json(force=True)
+
+  new_content = rqt["content"]
+
+  comment.content = new_content
+  
+  db.session.commit()
+  print(comment_schema.jsonify(comment))
+  return comment_schema.jsonify(comment) 
+
+@app.route("/comments", methods=["GET"])
+def get_comments():
+  all_comments = Comment.query.all()
+  result = comments_schema.dump(all_comments)
+  return jsonify(result)
+
+@app.route("/add-comment/<id>", methods=["POST"])
+def add_comment(id):
+  rqt = request.get_json(force=True)
+
+  user_id = rqt["user_id"]
+  post_id = rqt["post_id"]
+  content = rqt["content"]
+
+  record = Comment(user_id, post_id, content)
+
+  db.session.add(record)
+  db.session.commit()
+
+  comment = Comment.query.get(record.id)
+  return comment_schema.jsonify(comment)
+
+# POSTS
 
 @app.route("/post/<id>", methods=["GET"])
 def get_post(id):
@@ -99,7 +218,7 @@ def delete_post(id):
   db.session.delete(record)
   db.session.commit()
 
-  return jsonify("RECORD DELETED")
+  return jsonify("POST DELETED")
 
 if __name__ == '__main__':
   db.create_all()
