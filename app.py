140cc6d69d0589d4a0dae557d410ac121312f02a
Danielle Smith <daniellesmith@Danielles-MacBook-Pro.local>
2020-12-28 11:08:29 -0700

    simplified db

--- app.py
+++ app.py
@@ -1,74 +1,132 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, session, request, jsonify
+# from flask_session import session
+from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy  
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow 
+from flask_admin.contrib.sqla import ModelView 
+from flask_basicauth import BasicAuth
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
 import marshmallow_sqlalchemy
+import requests
 import os 
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# app.config['BASIC_AUTH_FORCE'] = True
+# app.config['BASIC_AUTH_USERNAME'] = 'Danielle'
+# app.config['BASIC_AUTH_PASSWORD'] = 'password'
+# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+# basic_auth = BasicAuth(app)
+# admin = Admin(app)
+flask_bcrypt = Bcrypt(app)
+# admin.add_view(ModelView(User, db.session))
+# admin.add_view(ModelView(Post, db.session))
+app.secret_key = os.environ.get('SECRET_KEY')
+app.permanent_session_lifetime = timedelta(minutes=5)
+CORS(app, supports_credentials=True)
+
 
 CORS(app)
 
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(50))
+  author = db.Column(db.String(25))
   content = db.Column(db.Text)
 
-  def __init__(self, title, content):
+  def __init__(self, title, author, content):
     self.title = title
-    self.content = content
-
-class Comment(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer)
-  content = db.Column(db.Text)
-  post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-
-  def __init__(self, user_id, post_id, content):
-    self.user_id = user_id
-    self.post_id = post_id
+    self.author = author
     self.content = content
   
-
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(25))
-  email = db.Column(db.String(40))
   password = db.Column(db.String(40))
-  admin = db.Column(db.Boolean)
+ 
 
-  def __init__(self, name, email, password, admin):
+  def __init__(self, name, password):
     self.name = name
-    self.email = email
     self.password = password
-    self.admin = admin
+  
   
 
 class PostSchema(ma.Schema):
   class Meta:
-    fields = ("id", "title", "content", "user_id", "post_id", "post", "name", "comment")
-
-class CommentSchema(ma.Schema):
-  class Meta:
-    fields = ("id", "user_id", "post_id", "content")
+    fields = ("id", "title", "author", "content")
 
 class UserSchema(ma.Schema):
   class Meta:
-    fields = ("id", "name", "email", "password", "admin")
+    fields = ("id", "name", "password")
 
 post_schema = PostSchema()
 posts_schema = PostSchema(many=True)
 
-comment_schema = CommentSchema()
-comments_schema = CommentSchema(many=True)
-
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+
+
+# @app.route('/secret')
+# @basic_auth.required
+# def secret_view():
+#   return "login"
+  
+
+@app.route("/")
+def hello():
+  return "hello"
+
+@app.route('/logged-in', methods=['GET'])
+def logged_in():
+  print(session)
+  if 'name' in session:
+    db_user: User.query.filter_by(name=session['name']).first()
+    print(db_user)
+    if db_user: 
+      return jasonify('User Loggedin Via Cookie')
+    else: 
+      return jsonify('Session exists, but user does not exist (anymore)')
+  else: 
+    return jsonify('nope')
+
+@app.route('/login', methods=['POST'])
+def login():
+  post_data = request.get_json()
+  db_user = User.query.filter_by(name=post_data.get('name')).first()
+  if db_user is None: 
+    return jsonify('Name NOT found')
+  password = post_data.get('password')
+  db_user_hashed_password = db_user.password
+  valid_password = flask_bcrypt.check_password_hash(db_user_hashed_password, password)
+  if valid_password:
+    session.permanent = True
+    session['name'] = post_data.get('name')
+    return jsonify('User Verified')
+  return jsonify('Password is not corret')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+  post_data = request.get_json()
+  db_user = User.query.filter_by(name=post_data.get('name')).first()
+  if db_user:
+    return jsonify('Name taken')
+  name = post_data.get('name')
+  email = post_data.get('email')
+  password = post_data.get('password')
+  hashed_password = flask_bcrypt.generate_password_hash(password).decode('utf-8') 
+  new_user = User(name=name, password=hashed_password)
+  db.session.add(new_user)
+  db.session.commit()
+  session.permanent = True
+  session['name'] = post_data.get('name')
+  return jsonify("User Created!")
 
 
 # USERS
@@ -123,50 +181,6 @@ def add_user():
   user = User.query.get(record.id)
   return user_schema.jsonify(user)
 
-# COMMENTS
-
-@app.route("/comment/<id>", methods=["DELETE"])
-def delete_comment(id):
-  record = Comment.query.get(id)
-  db.session.delete(record)
-  db.session.commit()
-
-  return jsonify("COMMENT DELETED")
-
-@app.route("/comment/<id>", methods=["PATCH"])
-def update_comment(id):
-  comment = Comment.query.get(id)
-  rqt = request.get_json(force=True)
-
-  new_content = rqt["content"]
-
-  comment.content = new_content
-  
-  db.session.commit()
-  print(comment_schema.jsonify(comment))
-  return comment_schema.jsonify(comment) 
-
-@app.route("/comments", methods=["GET"])
-def get_comments():
-  all_comments = Comment.query.all()
-  result = comments_schema.dump(all_comments)
-  return jsonify(result)
-
-@app.route("/add-comment/<id>", methods=["POST"])
-def add_comment(id):
-  rqt = request.get_json(force=True)
-
-  user_id = rqt["user_id"]
-  post_id = rqt["post_id"]
-  content = rqt["content"]
-
-  record = Comment(user_id, post_id, content)
-
-  db.session.add(record)
-  db.session.commit()
-
-  comment = Comment.query.get(record.id)
-  return comment_schema.jsonify(comment)
 
 # POSTS
 
@@ -187,9 +201,10 @@ def add_post():
   rqt = request.get_json(force=True)
   
   title = rqt["title"]
+  author = rqt["author"]
   content = rqt["content"]
 
-  record = Post(title, content)
+  record = Post(title, author, content)
   
   db.session.add(record)
   db.session.commit()
@@ -203,9 +218,11 @@ def update_post(id):
   rqt = request.get_json(force=True)
 
   new_title = rqt["title"]
+  new_author = rqt["author"]
   new_content = rqt["content"]
 
   post.title = new_title
+  post.author = new_author
   post.content = new_content
   
   db.session.commit()
